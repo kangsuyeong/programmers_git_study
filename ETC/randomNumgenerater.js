37cd52436aed3de2aae68b06e306b3f8fe1915bc
최민혁 <130155327+lactofreee@users.noreply.github.com>
2024-11-01 18:52:08 +0900

    (test) 추가 : 랜덤 뽑기 코드

---
+++ ETC/randomNumgenerater.js
@@ -0,0 +1,7 @@
+function pickRandomPerson() {
+  const people = ["윤슬", "민혁", "수영", "예지", "지운"];
+  const randomIndex = Math.floor(Math.random() * people.length);
+  return people[randomIndex];
+}
+
+console.log(pickRandomPerson());
